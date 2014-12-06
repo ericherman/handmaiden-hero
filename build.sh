106e01ad6a2bbb4caa6cfc1ecc730d956a22f097
Eric Herman <eric@freesa.org>
2014-12-06 17:44:08 +0100

    create a blank window in handmaiden, and exit

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ unset CFLAGS
 unset CXXFLAGS
 
 gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
- -o gebouwd/handmaiden src/handmaiden.c
+ -o gebouwd/handmaiden src/handmaiden.c \
+ -lX11
 
 gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
  -o gebouwd/x11-example src/x11-example.c \
--- src/handmaiden.c
+++ src/handmaiden.c
@@ -1,14 +1,57 @@
-#include <stdio.h>
+#include <X11/Xlib.h>
+
+#include <stdio.h>		/* fprintf */
+#include <unistd.h>		/* sleep */
 
 int main(int argc, char *argv[])
 {
-	char *name;
+	char *display_name;
+	Display *display;
+	Window parent;
+	int screen, x, y;
+	unsigned int width, height, border_width, border;
+	unsigned long background;
+	Window window;
 
 	if (argc > 1) {
-		name = argv[1];
+		display_name = argv[1];
 	} else {
-		name = "world";
+		display_name = NULL;
 	}
-	printf("Hello, %s.\n", name);
+
+	display = XOpenDisplay(display_name);
+	if (!display) {
+		fprintf(stderr, "Cannot open display '%s'\n", display_name);
+		return 1;
+	}
+	screen = DefaultScreen(display);
+	parent = RootWindow(display, screen);
+	x = 0;
+	y = 0;
+	width = 300;
+	height = 300;
+	border_width = 1;
+	border = BlackPixel(display, screen);
+	background = WhitePixel(display, screen);
+
+	window =
+	    XCreateSimpleWindow(display, parent, x, y, width, height,
+				border_width, border, background);
+
+	if (!window) {
+		fprintf(stderr, "Cannot XCreateSimpleWindow\n");
+		return 1;
+	}
+
+	/* let us accept keypresses */
+	XSelectInput(display, window, ExposureMask | KeyPressMask);
+
+	/* the window becomes viewable */
+	XMapWindow(display, window);
+
+	XFlush(display);
+
+	sleep(1);
+
 	return 0;
 }
