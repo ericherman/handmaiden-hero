9038419b0d19118c3316674883537058eaf9cd8b
Eric Herman <eric@freesa.org>
2014-12-06 12:49:02 +0100

    add x11-example to build.sh

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 mkdir -p build
 gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
  -o build/handmaiden src/handmaiden.c
+
+gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
+ -o build/x11-example src/x11-example.c \
+ -lX11
