81725c95bf07fbe8816dfd1c36ebfa50af486a27
Eric Herman <eric@freesa.org>
2014-12-06 20:02:45 +0100

    remove the x11-example.c from JeGX at geeks3d.com; not going to use this

--- build.sh
+++ build.sh
@@ -8,7 +8,3 @@ unset CXXFLAGS
 gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
  -o gebouwd/handmaiden src/handmaiden.c \
  -lX11
-
-gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
- -o gebouwd/x11-example src/x11-example.c \
- -lX11
