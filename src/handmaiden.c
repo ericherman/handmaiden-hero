9723419f40b7c724fa1c120075b8b89688fcace3
Eric Herman <eric@freesa.org>
2014-12-07 10:47:13 +0100

    parity with Handmade Hero Day 003 - Allocating a Backbuffer
    Since X11 does not have bitmap scaling function that I can find,
    I did this by hand, if crudely.
    
    Also, there is something amiss with redraw being called far too many times,
    and all of the redraw requests seemingly being queued up before the keypress,
    or close button ClientMessage events.
    
    But, having looked at the on-line docs, manpages, X11 sources, hacking in
    some debug info, and even scanning "Xlib − C Language X Interface", I still
    fail to have a useful clue about what is going on here.

--- src/handmaiden.c
+++ src/handmaiden.c
@@ -1,22 +1,53 @@
+#define HANDMAIDEN_TRY_TO_MAKE_VALGRIND_HAPPY 0
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
 #include <stdio.h>		/* fprintf */
+#include <stdlib.h>		/* malloc */
 
 #define BUF_LEN 255
 
-void redraw(Display * display, Window window, GC context,
-	    unsigned long foreground)
+/*
+#define internal_function static
+#define global_variable static
+#define local_persistant static
+*/
+
+struct virtual_window {
+	int height;
+	int width;
+	unsigned int *pixels;
+};
+
+void redraw(Display *display, Window window, GC context,
+	    struct virtual_window *virtual_win, int *shutdown)
 {
 	XWindowAttributes window_attributes;
-	int x, y;
+	int x, y, vert_y, vert_x, offset;
+	float x_ratio, y_ratio;
+	unsigned int foreground;
+
 	XGetWindowAttributes(display, window, &window_attributes);
-	for (x = 0; x < window_attributes.width; x++) {
-		for (y = 0; y < window_attributes.height; y++) {
+
+	y_ratio = virtual_win->height / (float) window_attributes.height;
+	x_ratio = virtual_win->width / (float) window_attributes.width;
+
+	for (y = 0; y < window_attributes.height; y++) {
+		vert_y = (int)(y * y_ratio);
+		for (x = 0; x < window_attributes.width; x++) {
+			if (*shutdown) {
+				fprintf(stderr, "incomplete redraw\n");
+				return;
+			}
+			vert_x = (int)(x * x_ratio);
+			offset = (vert_y * virtual_win->width) + vert_x;
+			foreground = *(virtual_win->pixels + offset);
 			XSetForeground(display, context, foreground);
 			XDrawPoint(display, window, context, x, y);
 		}
 	}
+	fprintf(stderr, "redrawn\n");
 }
 
 int main(int argc, char *argv[])
@@ -24,17 +55,18 @@ int main(int argc, char *argv[])
 	char *display_name, buf[BUF_LEN];
 	Display *display;
 	Window parent;
+	struct virtual_window *virtual_win;
 	int screen, x, y, shutdown, len;
-	unsigned int width, height, border_width, border;
-	unsigned long background, foreground, white, black;
+	unsigned int width, height, border_width, border, foreground;
+	unsigned long background, black;
 	Window window;
 	Bool only_if_exists;
 	Atom WM_DELETE_WINDOW;
-	XEvent event;
-	KeySym keysym;
 	GC context;
 	unsigned long valuemask;
 	XGCValues *values;
+	XEvent event;
+	KeySym keysym;
 
 	if (argc > 1) {
 		display_name = argv[1];
@@ -44,28 +76,44 @@ int main(int argc, char *argv[])
 
 	display = XOpenDisplay(display_name);
 	if (!display) {
-		fprintf(stderr, "Cannot open display '%s'\n", display_name);
+		fprintf(stderr, "Could not open display '%s'\n", display_name);
 		return 1;
 	}
 	screen = DefaultScreen(display);
 	parent = RootWindow(display, screen);
 	x = 0;
 	y = 0;
-	width = 300;
-	height = 300;
+	width = 255;
+	height = 255;
 	border_width = 1;
-	white = WhitePixel(display, screen);
 	black = BlackPixel(display, screen);
 	border = black;
-	background = white;
-	foreground = black;
+	background = black;
 
-	window =
-	    XCreateSimpleWindow(display, parent, x, y, width, height,
-				border_width, border, background);
+	virtual_win = malloc(sizeof(struct virtual_window));
+	if (!virtual_win) {
+		fprintf(stderr, "Could not malloc virtual_window\n");
+		return 1;
+	}
+	virtual_win->height = height;
+	virtual_win->width = width;
+	virtual_win->pixels = malloc(virtual_win->height * virtual_win->width * sizeof(unsigned int));
+	if (!virtual_win->pixels) {
+		fprintf(stderr, "Could not malloc virtual_win->pixels\n");
+		return 1;
+	}
+
+	for (y = 0; y < virtual_win->height; y++) {
+		for (x = 0; x < virtual_win->width; x++) {
+			foreground = ((x % 256) << 16) + (y % 256);
+			*(virtual_win->pixels + (y * virtual_win->width) + x) = foreground;
+		}
+	}
+
+	window = XCreateSimpleWindow(display, parent, x, y, width, height, border_width, border, background);
 
 	if (!window) {
-		fprintf(stderr, "Cannot XCreateSimpleWindow\n");
+		fprintf(stderr, "Could not XCreateSimpleWindow\n");
 		return 1;
 	}
 
@@ -89,8 +137,7 @@ int main(int argc, char *argv[])
 		XNextEvent(display, &event);
 		switch (event.type) {
 		case Expose:
-			foreground = (foreground == white) ? black : white;
-			redraw(display, window, context, foreground);
+			redraw(display, window, context, virtual_win, &shutdown);
 			break;
 		case KeyPress:
 			buf[0] = '\0';
@@ -103,11 +150,18 @@ int main(int argc, char *argv[])
 			if (len == 0) {
 				fprintf(stderr, "Zero length keypress.\n");
 			}
+			buf[len] = '\0';
+			fprintf(stderr, "keypress:'%s'\n", buf);
 			break;
 		case ClientMessage:
 			if ((unsigned int)(event.xclient.data.l[0]) ==
 			    (unsigned int)WM_DELETE_WINDOW) {
 				shutdown = 1;
+				fprintf(stderr, "WM_DELETE_WINDOW\n");
+			} else {
+				fprintf(stdout, "got ClientMessage: %u\n",
+					(unsigned int)(event.xclient.data.
+						       l[0]));
 			}
 			break;
 		default:
@@ -117,8 +171,13 @@ int main(int argc, char *argv[])
 	}
 
 	/* we probably do not need to do these next steps */
-	XDestroyWindow(display, window);
-	XCloseDisplay(display);
+	if (HANDMAIDEN_TRY_TO_MAKE_VALGRIND_HAPPY) {
+		free(virtual_win->pixels);
+		free(virtual_win);
+		XFreeGC(display, context);
+		XDestroyWindow(display, window);
+		XCloseDisplay(display);
+	}
 
 	return 0;
 }
