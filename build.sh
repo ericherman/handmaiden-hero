cc279687cb4e9f71ed062a619df150e218dd90c1
Eric Herman <eric@freesa.org>
2014-12-07 16:19:10 +0100

    compile with "gcc -O3"

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ mkdir -p gebouwd
 unset CFLAGS
 unset CXXFLAGS
 
-gcc -std=c89 -Os -Wall -Wextra -pedantic -Werror -ggdb \
+gcc -std=c89 -O3 -Wall -Wextra -pedantic -Werror -ggdb \
  -o gebouwd/handmaiden src/handmaiden.c \
  -lX11
