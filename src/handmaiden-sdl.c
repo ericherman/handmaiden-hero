49bf628ab763b6bb4c62dcbd84a48b7eaeb1dfc6
Eric Herman <eric@freesa.org>
2014-12-14 15:30:23 +0100

    create a simple SDL window and event loop

--- src/handmaiden-sdl.c
+++ src/handmaiden-sdl.c
@@ -3,13 +3,47 @@
 int main(int argc, char *argv[])
 {
 	char *title;
-	char *msg;
 	SDL_Window *win;
+	int x, y, width, height;
+	Uint32 flags;
+	int shutdown;
+	SDL_Event event;
+
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		return 1;
+	}
 
 	title = (argc > 1) ? argv[1] : "Handmaiden Hero";
-	msg = (argc > 2) ? argv[2] : "Simple Directmedia Library";
-	win = 0;
+	x = SDL_WINDOWPOS_UNDEFINED;
+	y = SDL_WINDOWPOS_UNDEFINED;
+	height = 480;
+	width = 640;
+	flags = SDL_WINDOW_RESIZABLE;
 
-	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, title, msg, win);
+	win = SDL_CreateWindow(title,x,y,width, height,flags);
+	if (!win) {
+		return 2;
+	}
+
+	shutdown = 0;
+	while (!shutdown) {
+		SDL_WaitEvent(&event);
+		switch(event.type) {
+		case SDL_QUIT:
+			shutdown = 1;
+			break;
+		case SDL_WINDOWEVENT:
+			switch(event.window.event) {
+			case SDL_WINDOWEVENT_RESIZED:
+				break;
+			default:
+				break;
+			}
+			break;
+		default:
+			break;
+		}
+	}
 	return 0;
 }
